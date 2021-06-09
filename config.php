15127aca38a9e332feb132ac428da8c3201fc9f4
iida-takuya <takuya.iida@tsukuba.pascalia.co.jp>
2021-06-09 10:29:47 +0900

    register and login

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// File: config.php
+// Below information is only an example, you need to change to correct information on you local environment. 
+$dbConfig = [
+    "host" => "localhost",
+    "user" => "root",
+    "password" => "",
+    "db_name" => "php_web"
+];
--- modules/partials/header.php
+++ modules/partials/header.php
@@ -1,11 +1,30 @@
 <?php
-// Define an array to contain page titles
-$pageTitles = array(
-    'home' => "Home",
-    "profile" => "My Profile",
-);
-// Get page title depend on what is using module. 
-$pageTitle = $pageTitles[$module];
+    // Define an array to contain page titles
+    $pageTitles = array(
+        'home' => "Home",
+        "profile" => "My Profile",
+        "register" => "Regiser"
+    );
+    // Get page title depend on what is using module
+    $pageTitle = array_key_exists($module, $pageTitles) ? $pageTitles[$module] : null;
+
+    // Get session for checking user logged-in or not
+    $userId = array_key_exists('login_user_id', $_SESSION) ? $_SESSION['login_user_id'] : null;
+    // Default, user is not logged-in
+    $user = false;
+    if ($userId) {
+        // query user data by $username and $password
+        $sql = "SELECT id, username, email, fullname 
+            FROM users 
+            WHERE id = $userId
+            LIMIT 0,1";
+
+        $result = $mysql->query($sql);
+        $user = $result->fetch_array() ?? false;
+    }
+
+    // Define fullname to show on header
+    $fullname = $user ? $user['fullname'] : 'Guest';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,24 +36,28 @@ $pageTitle = $pageTitles[$module];
         <link rel="stylesheet" href="./assets/css/index.css" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 
-        <script src="./assets/js/index.js"></script>s
+        <script src="./assets/js/index.js"></script>
     </head>
     <body>
         <!-- The Header -->
-        <header> 
+        <header>
         <div>
-            <h4>The logo</h4>
+            <?php if(array_key_exists('login_user_id', $_SESSION)): ?>
+                <h4>The logo<?php echo $_SESSION['login_user_id']; ?> </h4>
+            <?php else: ?>
+                <h4>The logo</h4>
+            <?php endif ?>
         </div>
         <div>
                 <h2 class="slogan">The header slogan</h2>
         </div>
         <div id="form">
             <ul>
-                <li>Hi <span>Guest</span></li>
+                <li>Hi <span><?php echo $fullname; ?></span></li>
                 <li><a href="javascript:void(0)" onclick="showLoginForm()">Login</a></li>
             </ul>
-            
-            <form id="login">
+
+            <form id="login" action="index.php?m=login" method="post">
                 <input type="text" name="username" placeholder="User name" />
                 <input type="password" name="password" placeholder="Password"/>
                 <label><input type="checkbox" name="rememberUsername" />Remember user name </label>
@@ -46,11 +69,12 @@ $pageTitle = $pageTitles[$module];
             </form>
         </div>
         </header>
-        
+
         <!-- The menu -->
         <nav>
             <ul>
                 <li><a href="./index.php">Home</a></li>
+                <li><a href="./index.php?m=register">Register</a></li>
                 <li><a href="./index.php?m=profile">My Profile</a></li>
             </ul>
         </nav>
